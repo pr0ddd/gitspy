0f5104d36658462a631d452a03349fcd956cf0f0
pr0d <pavel.erohovets@gmail.com>
2026-08-05 20:14:50 +0300

    Updater and process plugins with R2 endpoint placeholder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,6 +19,8 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .manage(state::AppState::default())
         .manage(hosts::Hosts::default())
         .invoke_handler(tauri::generate_handler![
